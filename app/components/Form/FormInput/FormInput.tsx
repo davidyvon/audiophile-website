9601c83116f3e900a177ef27b88dbe814dadfd2e
David Yvon <david.yvon@pelostud.io>
2023-01-04 12:50:44 +0100

    Add <FormInput/>

---
+++ app/components/Form/FormInput/FormInput.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import { storyblokEditable } from '@storyblok/react'
+import styles from './FormInput.module.scss'
+import classNames from 'classnames'
+
+type FormInputProps = {
+	blok: {
+		component: string
+		_uid: string
+		_editable?: string
+
+		type?: 'text' | 'email' | 'tel'
+		required?: boolean
+		label?: string
+		name?: string
+		placeholder?: string
+		errorMessage?: string
+	}
+
+	hasError?: boolean
+	className?: string
+	value?: any
+	onChange: (name: string, value: any) => void
+}
+
+const FormInput = ({
+	hasError = false,
+	className,
+	blok,
+	value,
+	onChange,
+}: FormInputProps): JSX.Element => {
+	const { type, required, label, name, placeholder, errorMessage } = blok
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		name && onChange(name, event.target.value)
+	}
+
+	const sharedClassNames = classNames(className, {
+		[styles.hasError]: hasError,
+	})
+
+	return (
+		<>
+			{label && name && placeholder && type && value && (
+				<div className={styles.container} {...storyblokEditable(blok)}>
+					<label
+						className={classNames(styles.label, sharedClassNames)}
+						htmlFor={name}
+					>
+						<div className={styles.name}>{label}</div>
+						<div className={classNames(styles.error, sharedClassNames)}>
+							{errorMessage}
+						</div>
+					</label>
+					<input
+						type={type}
+						required={required}
+						id={name}
+						name={name}
+						placeholder={placeholder}
+						value={value[name] || ''}
+						onChange={handleChange}
+						className={classNames(styles.input, sharedClassNames)}
+					/>
+				</div>
+			)}
+		</>
+	)
+}
+
+export default FormInput
