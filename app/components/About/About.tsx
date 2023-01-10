ae217878b1f1857f0856504f3c7fc10bbcb0dc7b
David Yvon <david.yvon@pelostud.io>
2023-01-10 19:16:20 +0100

    Add <RevealOnScroll/>

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -3,6 +3,7 @@ import { storyblokEditable } from '@storyblok/react'
 import { render } from 'storyblok-rich-text-react-renderer-ts'
 import Image from 'next/image'
 import styles from './About.module.scss'
+import RevealOnScroll from '../../animations/RevealOnScroll'
 
 type AboutProps = {
 	blok: {
@@ -31,46 +32,48 @@ const About = ({ blok }: AboutProps): JSX.Element => {
 	const { imageDesktop, imageTablet, imageMobile, heading, description } = blok
 
 	return (
-		<section className={styles.section} {...storyblokEditable(blok)}>
-			<div className={styles.container}>
-				<div className={styles.picture}>
-					{imageDesktop && imageDesktop.filename && (
-						<Image
-							className={styles.imageDesktop}
-							src={imageDesktop.filename}
-							alt={imageDesktop.alt}
-							width={540}
-							height={588}
-						/>
-					)}
+		<RevealOnScroll>
+			<section className={styles.section} {...storyblokEditable(blok)}>
+				<div className={styles.container}>
+					<div className={styles.picture}>
+						{imageDesktop && imageDesktop.filename && (
+							<Image
+								className={styles.imageDesktop}
+								src={imageDesktop.filename}
+								alt={imageDesktop.alt}
+								width={540}
+								height={588}
+							/>
+						)}
 
-					{imageTablet && imageTablet.filename && (
-						<Image
-							className={styles.imageTablet}
-							src={imageTablet.filename}
-							alt={imageTablet.alt}
-							width={1378}
-							height={600}
-						/>
-					)}
+						{imageTablet && imageTablet.filename && (
+							<Image
+								className={styles.imageTablet}
+								src={imageTablet.filename}
+								alt={imageTablet.alt}
+								width={1378}
+								height={600}
+							/>
+						)}
 
-					{imageMobile && imageMobile.filename && (
-						<Image
-							className={styles.imageMobile}
-							src={imageMobile.filename}
-							alt={imageMobile.alt}
-							width={654}
-							height={600}
-						/>
-					)}
+						{imageMobile && imageMobile.filename && (
+							<Image
+								className={styles.imageMobile}
+								src={imageMobile.filename}
+								alt={imageMobile.alt}
+								width={654}
+								height={600}
+							/>
+						)}
+					</div>
+
+					<article className={styles.content}>
+						{heading && <div className={styles.heading}>{render(heading)}</div>}
+						{description && <p className={styles.description}>{description}</p>}
+					</article>
 				</div>
-
-				<article className={styles.content}>
-					{heading && <div className={styles.heading}>{render(heading)}</div>}
-					{description && <p className={styles.description}>{description}</p>}
-				</article>
-			</div>
-		</section>
+			</section>
+		</RevealOnScroll>
 	)
 }
 
--- app/components/CardSmall/CardSmall.tsx
+++ app/components/CardSmall/CardSmall.tsx
@@ -5,6 +5,7 @@ import {
 	SbBlokData,
 } from '@storyblok/react'
 import styles from './CardSmall.module.scss'
+import RevealOnScroll from '../../animations/RevealOnScroll'
 
 type CardSmallProps = {
 	blok: {
@@ -20,16 +21,18 @@ const CardSmall = ({ blok }: CardSmallProps): JSX.Element => {
 	const { cards } = blok
 
 	return (
-		<section className={styles.section} {...storyblokEditable(blok)}>
-			<div className={styles.container}>
-				<div className={styles.cards}>
-					{cards &&
-						cards.map((card) => (
-							<StoryblokComponent key={card._uid} blok={card} />
-						))}
+		<RevealOnScroll>
+			<section className={styles.section} {...storyblokEditable(blok)}>
+				<div className={styles.container}>
+					<div className={styles.cards}>
+						{cards &&
+							cards.map((card) => (
+								<StoryblokComponent key={card._uid} blok={card} />
+							))}
+					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</RevealOnScroll>
 	)
 }
 
